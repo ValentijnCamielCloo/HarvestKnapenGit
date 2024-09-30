2e12c5d84cdd41733e33b8f2def6fc59fc200a1a
Sarah Edelaar <sarah.edelaar@gmail.com>
2024-09-30 23:01:49 +0200

    visualise-ply-test_30-09

---
+++ test_ply.py
@@ -0,0 +1,16 @@
+import open3d as o3d    #Version 0.18.0
+import numpy as np      #Version 1.26.4
+import copy
+
+ply_file_path = 'out-v.ply'
+
+print("Load a ply point cloud, print it, and render it")
+# ply_point_cloud = o3d.data.PLYPointCloud()
+pcd = o3d.io.read_point_cloud(ply_file_path)
+print(pcd)
+print(np.asarray(pcd.points))
+o3d.visualization.draw_geometries([pcd],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
